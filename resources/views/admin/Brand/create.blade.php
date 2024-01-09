b3a2bb0046bc44e88ecbf7fbb0fb320e25aa98aa
ikramulhasan1 <hasanikramul926@gmail.com>
2024-01-09 21:44:22 +0600

    Brand model, migration, controler, view file create

---
+++ resources/views/admin/Brand/create.blade.php
@@ -0,0 +1,103 @@
+@extends('admin.master')
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">					
+        <div class="container-fluid my-2">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Create Category</h1>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="{{ route('categories.index')}}" class="btn btn-primary">Back</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <!-- Default box -->
+        <div class="container-fluid">
+            <form action="{{route('categories.store')}}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="card">
+                    <div class="card-body">								
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="name" value="{{ old('name') }}" id="name" class="form-control" placeholder="Name">	
+                                    @error('name')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="name">Image</label>
+                                    <input type="file" name="images" id="inputImage" class="form-control @error('images') is-invalid @enderror">
+                                    @error('images')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+
+                                </div>
+                            </div>
+                        
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="email">Slug</label>
+                                    <input type="text" name="slug" disabled value="{{ old('slug') }}" id="slug" class="form-control" placeholder="Slug">	
+                                    @error('slug')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>									
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="status">Status</label>
+                                    <select name="status" id="status" class="form-control">
+                                        <option value="1">Active</option>
+                                        <option value="0">Block</option>
+                                    </select>
+                                    @error('status')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>									
+                        </div>
+                    </div>							
+                </div>
+                <div class="pb-5 pt-3">
+                    <button class="btn btn-primary">Create</button>
+                    <a href="brands.html" class="btn btn-outline-dark ml-3">Cancel</a>
+                </div>
+            </form>
+        </div>
+        <!-- /.card -->
+    </section>
+    <!-- /.content -->
+</div>
+@endsection
+
+@section('customJs')
+   {{-- <script>
+    var element = $(this);
+    $("#name").change(function(){
+        element = $(this);
+        $.ajax({
+            url: '{{ route("getSlug") }}',
+            type: 'get',
+            data: {title: element.val()},
+            dataType: 'json',
+            success: function (response) {
+                if (response["status"] == true) {
+                    $("#slug").val(response ["slug"])
+                }
+            }
+        });
+    });
+   </script> --}}
+@endsection
