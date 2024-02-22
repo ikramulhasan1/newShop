41fdb935687460a5bf8267c3297f417c1f1a1a9f
ikramulhasan1 <hasanikramul926@gmail.com>
2024-02-23 00:06:57 +0600

    LATEST PRODUSTS frontend page show

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -12,13 +12,23 @@ class FrontendController extends Controller
     {
         $categories = Category::orderBy('name', 'ASC')
             ->with('sub_category')
+            ->orderBy('id', 'DESC')
             // ->with('products')
             ->where('status', '1')
             ->where('showHome', 'Yes')
             ->get();
 
-        $products = Product::where('is_featured', 'Yes')->where('status', 1)
+        $products = Product::where('is_featured', 'Yes')
+            ->orderBy('id', 'DESC')
+            ->take(8)
+            ->where('status', 1)
             ->get();
-        return view('frontend.home', compact('categories', 'products'));
+
+        $latestProducts = Product::orderBy('id', 'DESC')
+            ->where('status', 1)
+            ->take(8)
+            ->get();
+
+        return view('frontend.home', compact('categories', 'products', 'latestProducts'));
     }
 }
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -378,7 +378,61 @@
                 <h2>Latest Produsts</h2>
             </div>
             <div class="row pb-3">
-                <div class="col-md-3">
+                @isset($latestProducts)
+                    @forelse ($latestProducts as $latestProduct)
+                        <div class="col-md-3">
+                            <div class="card product-card">
+                                <div class="product-image position-relative">
+                                    <a href="" class="product-img"><img class="card-img-top"
+                                            src="{{ 'storage/product-images/' . $latestProduct->image }}" alt=""></a>
+                                    <a class="whishlist" href="222"><i class="far fa-heart"></i></a>
+
+                                    <div class="product-action">
+                                        <a class="btn btn-dark" href="#">
+                                            <i class="fa fa-shopping-cart"></i> Add To Cart
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="card-body text-center mt-3">
+                                    <a class="h6 link" href="product.php">{{ $latestProduct->title }}</a>
+                                    <div class="price mt-2">
+                                        <span class="h5"><strong>{{ $latestProduct->price }}</strong></span>
+                                        @if ($latestProduct->compare_price > $latestProduct->price)
+                                            <span
+                                                class="h6 text-underline"><del>{{ $latestProduct->compare_price }}</del></span>
+                                        @endif
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @empty
+                        <div class="col-md-3">
+                            <div class="card product-card">
+                                <div class="product-image position-relative">
+                                    <a href="" class="product-img"><img class="card-img-top"
+                                            src="{{ asset('ui/frontend/images/defult/150x150.png') }}" alt=""></a>
+                                    <a class="whishlist" href="222"><i class="far fa-heart"></i></a>
+
+                                    <div class="product-action">
+                                        <a class="btn btn-dark" href="#">
+                                            <i class="fa fa-shopping-cart"></i> Add To Cart
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="card-body text-center mt-3">
+                                    <a class="h6 link" href="product.php">No Latest Produsts Available</a>
+                                    <div class="price mt-2">
+                                        <span class="h5"><strong>$100</strong></span>
+                                        <span class="h6 text-underline"><del>$120</del></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforelse
+                @endisset
+
+                {{-- <div class="col-md-3">
                     <div class="card product-card">
                         <div class="product-image position-relative">
                             <a href="" class="product-img"><img class="card-img-top"
@@ -531,29 +585,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card product-card">
-                        <div class="product-image position-relative">
-                            <a href="" class="product-img"><img class="card-img-top"
-                                    src="{{ asset('ui/frontend') }}/images/product-1.jpg" alt=""></a>
-                            <a class="whishlist" href="222"><i class="far fa-heart"></i></a>
-
-                            <div class="product-action">
-                                <a class="btn btn-dark" href="#">
-                                    <i class="fa fa-shopping-cart"></i> Add To Cart
-                                </a>
-                            </div>
-                        </div>
-                        <div class="card-body text-center mt-3">
-                            <a class="h6 link" href="product.php">Dummy Product Title</a>
-                            <div class="price mt-2">
-                                <span class="h5"><strong>$100</strong></span>
-                                <span class="h6 text-underline"><del>$120</del></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </section>
